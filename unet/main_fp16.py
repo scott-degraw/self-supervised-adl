Loss calculation fixes

unet/main_fp16.py
@@ -1,9 +1,12 @@
+from os import path 
+from os.path import join
+
+
 import torch
 import torch.nn as nn
 
 from torch.optim import Adam 
 from torch.cuda.amp import GradScaler, autocast
-from os.path import join
 
 from utils import * 
 from model import UNet
@@ -40,14 +43,14 @@ def epoch_step(train_dl:torch.utils.data.DataLoader, model:nn.Module, criterion:
         with autocast():
             outputs = model(inputs)
             loss = criterion(outputs, targets)
-            total_loss += loss.item()
+            total_loss += inputs.shape[0] * loss.item()
 
         # Backward pass and Optimize
         SCALER.scale(loss).backward()
         SCALER.step(optimizer)
         SCALER.update()
 
-    return total_loss
+    return total_loss / len(train_dl.dataset)
 
 
 def test_step(test_dl:torch.utils.data.DataLoader, model:nn.Module, criterion:nn.Module) -> float:
@@ -73,9 +76,8 @@ def test_step(test_dl:torch.utils.data.DataLoader, model:nn.Module, criterion:nn
                 outputs = model(inputs)
                 loss = criterion(outputs, targets)
                 total_loss += loss.item()
-    return total_loss
 
-
+    return total_loss / len(test_dl.dataset)
 
 if __name__ == '__main__':
     data_dir = '/home/squirt/Documents/data'
@@ -115,6 +117,8 @@ if __name__ == '__main__':
 
     # Save the model
     network = network.to(torch.device('cpu'), torch.float64)
+
+
     torch.save(network.state_dict(), 'unet_oxford.pth')
 
     print('Done')