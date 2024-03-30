8c31d30ae795a2fa6a81c4d3e0a51f76970c500c
Scott DeGraw <scott.degraw.23@ucl.ac.uk>
2024-03-30 12:05:07 +0000

    Moved segmentaiton train to special script

--- unet/segmentation_train.py
+++ unet/segmentation_train.py
@@ -1,30 +1,31 @@
-import torch
-import torch.nn as nn
-from torch.utils.data import random_split
-
-from torch.optim import Adam
-from torch.cuda.amp import GradScaler, autocast
+from os import path 
 from os.path import join
 
-from utils import *
+import PIL
+
+import torch
+import torchvision
+import torch.nn as nn
+
+from torch.optim import Adam 
+from torch.cuda.amp import GradScaler, autocast
+
+from torch.utils.data import random_split
+
+from utils import * 
 from model import UNet
 
 # Throw error if cuda not available (sorry mac people)
 assert torch.cuda.is_available(), "CUDA not available"
-DTYPE = None  # None gives default autocast behaviour
-DEVICE = torch.device("cuda")
+DEVICE = torch.device('cuda')
 SCALER = GradScaler()
 
 
-"""
+'''
 Training/Testing Loops
-"""
-
-
-def epoch_step(
-    train_dl: torch.utils.data.DataLoader, model: nn.Module, criterion: nn.Module, optimizer: torch.optim.Optimizer
-) -> float:
-    """
+''' 
+def epoch_step(train_dl:torch.utils.data.DataLoader, model:nn.Module, criterion:nn.Module, optimizer:torch.optim.Optimizer) -> float:
+    '''
     Do one epoch training Step
     Inputs:
         - train_dl (data.DataLoader): training dataloader
@@ -33,19 +34,19 @@ def epoch_step(
         - optimizer (optim.Optimizer): optimizer
     Returns:
         - total_loss: total loss for the epoch
-    """
-    total_loss = 0.0
+    '''
+    total_loss = 0.
     model.train()
-    for images, masks in train_dl:
-        images = images.to(DEVICE)
-        masks = masks.to(DEVICE)
+    for (inputs, targets) in train_dl:
+        inputs = inputs.to(DEVICE)
+        targets = targets.to(DEVICE)
 
         # Forward pass
         optimizer.zero_grad()
-        with autocast(dtype=DTYPE):
-            outputs = model(masks * images)
-            loss = criterion(outputs, images)
-            total_loss += images.shape[0] * loss.item()
+        with autocast():
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+            total_loss += inputs.shape[0] * loss.item()
 
         # Backward pass and Optimize
         SCALER.scale(loss).backward()
@@ -55,8 +56,8 @@ def epoch_step(
     return total_loss / len(train_dl.dataset)
 
 
-def test_step(test_dl: torch.utils.data.DataLoader, model: nn.Module, criterion: nn.Module) -> float:
-    """
+def test_step(test_dl:torch.utils.data.DataLoader, model:nn.Module, criterion:nn.Module) -> float:
+    '''
     Test using the validation/test set
     Inputs:
         - test_dl (data.DataLoader): test dataloader
@@ -64,22 +65,21 @@ def test_step(test_dl: torch.utils.data.DataLoader, model: nn.Module, criterion:
         - criterion (nn.Module): loss function
     Returns:
         Loss for the test set
-    """
-    total_loss = 0.0
+    '''
+    total_loss = 0.
     model.eval()
     with torch.no_grad():
-        for images, masks in test_dl:
-            images = images.to(DEVICE)
-            masks = masks.to(DEVICE)
+        for (inputs, targets) in test_dl:
+            inputs = inputs.to(DEVICE)
+            targets = targets.to(DEVICE)
 
             # Forward pass
-            with autocast(dtype=DTYPE):
-                outputs = model(masks * images)
-                loss = criterion(outputs, images)
-                total_loss += images.shape[0] * loss.item()
+            with autocast():
+                outputs = model(inputs)
+                loss = criterion(outputs, targets)
+                total_loss += targets.shape[0] * loss.item()
 
     return total_loss / len(test_dl.dataset)
-
 
 def train_loop(
     train_dl: DataLoader,
@@ -90,8 +90,8 @@ def train_loop(
     max_num_epochs: int = 10,
     patience: int = 5,
 ):
-    val_losses = []
-    best_val_loss = torch.inf
+    val_scores = []
+    best_val_score = torch.inf
     no_improvement_counter = 0
 
     model_state_dicts = []
@@ -100,57 +100,53 @@ def train_loop(
         # Train
         epoch_loss = epoch_step(train_dl, model, criterion, optim)
         # Test
-        val_loss = test_step(val_dl, model, criterion)
-        print(f"Epochs {epoch + 1} Loss: {epoch_loss:.4g} Val loss: {val_loss:.4g}")
+        val_score = model_iou(model, val_dl, DEVICE)
+        print(f"Epoch {epoch + 1} Loss: {epoch_loss:.4g} Val iou: {val_score:.4g}")
 
         # Early stopping
-        val_losses.append(val_loss)
+        val_scores.append(val_score)
 
         cpu_state_dict = {key: tensor.cpu() for key, tensor in model.state_dict().items()}
         model_state_dicts.append(cpu_state_dict)
 
-        if val_loss < best_val_loss:
+        if val_score > best_val_score:
             no_improvement_counter = 0
-            best_val_loss = val_loss
+            best_val_score = val_score
         else:
             no_improvement_counter += 1
 
         if no_improvement_counter == patience:
             break
 
-    best_epoch = torch.tensor(val_losses).argmax()
-    best_val_loss = val_losses[best_epoch]
+    best_epoch = torch.tensor(val_scores).argmax()
+    best_val_score = val_scores[best_epoch]
     model.load_state_dict(model_state_dicts[best_epoch])
 
-
-if __name__ == "__main__":
-    torch.manual_seed(438792)
-    batch_size = 25
-    eval_batch_size = 50
-    max_num_epochs = 10
+if __name__ == '__main__':
+    torch.manual_seed(879)
+    batch_size = 32
+    eval_batch_size = 64
+    max_num_epochs = 20
     patience = 5
-
-    square_size = 16
+    split = 0.8
 
     image_size = (240, 240)
 
     root_dir = "../data"
-    ds = SynthDataset(root_dir, image_size=image_size)
-    pretrain_ds = PretrainingDataset(ds, CheckerboardMask(square_size=square_size, image_size=ds.image_size))
+    train_val_ds = OxfordPetsDataset(root=root_dir, split="train", image_size=image_size)
+    test_ds = OxfordPetsDataset(root=root_dir, split="test", image_size=image_size)
 
-    split_frac = 0.8
-    train_ds, val_ds, test_ds = random_split(
-        pretrain_ds, [split_frac * split_frac, split_frac * (1 - split_frac), 1 - split_frac]
-    )
+    train_ds, val_ds = random_split(train_val_ds, [split, 1 - split])
+
     train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
     val_dl = DataLoader(val_ds, batch_size=eval_batch_size, shuffle=False)
     test_dl = DataLoader(test_ds, batch_size=eval_batch_size, shuffle=False)
 
-    # Model
-    model = UNet().to(DEVICE)
+    model = UNet(num_out_channels=1)
+    model = model.to(DEVICE, torch.float32)
 
     # Loss
-    criterion = nn.MSELoss()
+    criterion = nn.BCEWithLogitsLoss()
 
     # Optimizer
     lr = 1e-3
@@ -167,15 +163,18 @@ if __name__ == "__main__":
         patience=patience,
     )
 
+    # Save the model
+    model = model.to(torch.device('cpu'), torch.float64)
+
+    torch.save(model.state_dict(), 'unet_segmentation.pt')
+
     # Test model
-    test_loss = test_step(test_dl, model, criterion)
-    print(f"Test Loss: {test_loss:.4g}")
+    test_score = model_iou(model, test_dl, DEVICE)
+    print(f'Test Loss: {test_score}')
+
+    model.load_state_dict(torch.load('unet_segmentation.pt'))
 
     # Test output
-    save_image_output(model, test_dl, "pretrain_output.png", DEVICE)
+    segmentation_image_output(model, test_dl, 'oxford_output.png', DEVICE)
 
-    # Save the model
-    model = model.to(torch.device("cpu"), torch.float64)
-    torch.save(model.state_dict(), "unet_pets.pth")
-
-    print("Done")
+    print('Done')
